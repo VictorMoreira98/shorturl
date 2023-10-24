1596cb4b43e0c056b4237f794725f3ffad0d866d
Victor Moreira <victormoreiracorrea@gmail.com>
2023-10-24 18:16:40 -0300

    feat: add config db feat: endpoints feat: validate url

---
+++ utils/utils.js
@@ -0,0 +1,14 @@
+export function validateUrl(value) {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', // fragment locator
+    'i'
+  );
+    return pattern.test(
+      value
+    );
+  }
